e6111596384109b6d07e08b112559a5c7577c7a3
Yavuz Tokus <yavuztokus2010@gmail.com>
2021-11-12 22:50:40 +0000

    Added feature  user details show-hide

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 const Dashboard = ({ auth: { user } }) => {
+  const [userDetail, setUserDetail] = useState(false);
+
   return (
-    <div style={{ marginTop: "5rem", textAlign: "center" }}>
+    <div style={{ marginTop: "8rem", textAlign: "center" }}>
       <h1>Welcome, {user && user.name}</h1>
+      <div style={{ marginTop: "2rem" }}>
+        <button className="btn btn-primary" onClick={()=> setUserDetail(!userDetail)}>{userDetail ? "Hide your details" : "Show your details"}</button>
+      </div>
+     {userDetail && 
+      <div style={{ marginTop: "1rem" }}>
+        <p>Name: {user.name}</p>
+        <p>Email Address: {user.email}</p>
+      </div>}
     </div>
   );
 };
