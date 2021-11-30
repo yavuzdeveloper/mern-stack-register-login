e09117d565e1d521760373022f0407395e5ec8b7
Yavuz Tokus <yavuztokus2010@gmail.com>
2021-11-30 20:13:11 +0000

    refactoring

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db.js");
-const favicon = require("serve-favicon");
-const path = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
@@ -13,8 +11,6 @@ const app = express();
 connectDB();
 
 const PORT = process.env.PORT || 8000;
-
-app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 app.use(cors());
 
